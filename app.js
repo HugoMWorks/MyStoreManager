9cb019ec4c4a5f6224eaf8399941d017041726e5
HugoMWorks <h.morillon@outlook.com>
2020-05-22 18:29:52 +0200

    ajout de la connexion à la bdd

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // Modules externes
-const express = require('express')
-const mysql = require('promise-mysql')
-const morgan = require('morgan')('dev')
-const bodyParser = require('body-parser')
+const express = require("express");
+const mysql = require("promise-mysql");
+const morgan = require("morgan")("dev");
+const bodyParser = require("body-parser");
 
 // Modules internes
-const config = require('./config')
+const config = require("./config");
 
-// Variables globales
-let app = express()
+// Connexion à la base de données
+mysql
+  .CreateConnection({
+    host: config.db.hostname,
+    user: config.db.user,
+    password: config.db.password,
+    database: config.db.database,
+  })
+  .then(async (db) => {
+    // Variables globales
+    let app = express();
 
+    /* ROUTES  */
 
-/* ROUTES  */
+    app.get(config.rootAPI, (req, res) => {
+      res.send("It Works");
+    });
 
-app.get(config.rootAPI, (req, res) => {
-    res.send('It Works')
-})
+    // Lancement de l'API
 
-// Lancement de l'API
-
-app.listen(config.port, () => console.log('API started on port '+config.port))
+    app.listen(config.port, () =>
+      console.log("API started on port " + config.port)
+    );
+  })
+  .catch((err) => {
+    console.log("Erreur lors de la connexion à la base de données");
+    console.log(err.message);
+  });
